d8fe871d43d3e3d107ab0e6de90db1864843cffb
jparaducha <paraducha_97@hotmail.com>
2022-09-21 14:27:01 -0300

    jackson parser for flights

--- java/src/main/java/xml/JacksonParser.java
+++ java/src/main/java/xml/JacksonParser.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jdbc.City;
 import jdbc.Country;
-import jdbc.DAO.mysql.CountryDAO;
+import jdbc.DAO.mysql.FlightDAO;
 import jdbc.Flight;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -36,11 +36,11 @@ public class JacksonParser {
         File flightJson = new File("java\\src\\main\\resources\\json\\flight.json");
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); //
 
-        Country city4 = new CountryDAO().getById(4);
+        Flight flight4 = new FlightDAO().getById(4);
 
         City city1 = objectMapper.readValue(cityJson, City.class);
 
-        objectMapper.writeValue(new File("java\\src\\main\\resources\\json\\cityTest4.json"), city4);
+        objectMapper.writeValue(new File("java\\src\\main\\resources\\json\\flightTest.json"), flight4);
 
         Flight flight = objectMapper.readValue(flightJson, Flight.class);
 
