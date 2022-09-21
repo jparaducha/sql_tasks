02fbff60ce3b3ff593962fe479bd97362d2a7830
jparaducha <paraducha_97@hotmail.com>
2022-09-21 00:30:31 -0300

    jackson parser initial commit

---
+++ java/src/main/java/xml/JacksonParser.java
@@ -0,0 +1,44 @@
+package xml;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdbc.City;
+import jdbc.Country;
+import jdbc.Flight;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.IOException;
+
+public class JacksonParser {
+
+    private static final Logger LOGGER = LogManager.getLogger(JacksonParser.class);
+
+    public static void main(String[] args) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Country argentina = new Country();
+        argentina.setCountryName("Argentina");
+        argentina.setCountryId(1986);
+
+        City city = new City();
+        city.setCityName("P.R. Sáenz Peña");
+        city.setCityId(1915);
+        city.setCountry(argentina);
+
+        objectMapper.writeValue(new File("java\\src\\main\\resources\\json\\cityTest.json"), city);
+
+        File cityJson = new File("java\\src\\main\\resources\\json\\city.json");
+        File flightJson = new File("java\\src\\main\\resources\\json\\flight.json");
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); //
+
+        City city1 = objectMapper.readValue(cityJson, City.class);
+
+        Flight flight = objectMapper.readValue(flightJson, Flight.class);
+
+        LOGGER.info(city1);
+        LOGGER.info(flight);
+    }
+}
